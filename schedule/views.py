23f58f22296aabae8d7f2a9972f2aa27a646aa3d
Артамонов Кирилл Александрович <artamonovka@student.bmstu.ru>
2025-03-22 18:41:08 +0300

    actual-2

--- schedule/views.py
+++ schedule/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .serializers import LessonSerializer
 
 from .models import Lesson
 
 
 class LessonView(APIView):
     def get(self, request):
-        lessons = Lesson.objects.all()
-        return Response({"lessons": lessons})
+        my_header = request.headers.get('id')
+        lesson = Lesson.objects.filter(id = my_header)
+        serializer = LessonSerializer(lesson, many=True)  
+        return Response({"lesson": serializer.data})
+#        return Response({"lessons": lessons})
