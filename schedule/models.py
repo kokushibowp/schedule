62693c096724be953138529974db83e44b180c77
Артамонов Кирилл Александрович <artamonovka@student.bmstu.ru>
2025-03-22 17:59:12 +0300

    actual

--- schedule/models.py
+++ schedule/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class Lesson(models.Model):
+    id = models.IntegerField(primary_key=True)
     discipline = models.CharField(max_length=240)
-    time = models.CharField(max_length=20)
     teachername = models.CharField(max_length=120)
     numclassroom = models.IntegerField()
+    time = models.TimeField()
 
     def __str__(self):
         return self.discipline
